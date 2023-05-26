df5ba4fdf964290fa6b1ab2cec172a8d904df898
xieliangji <450324623@qq.com>
2023-05-26 23:05:37 +0800

    [IMP] add quickstart test \n [POM] add commons-lang3 dependecy

--- src/main/java/xieliangji/playwright/examples/Quickstart.java
+++ src/main/java/xieliangji/playwright/examples/Quickstart.java
@@ -1,2 +1,8 @@
-package xieliangji.playwright.examples;public class Quickstart {
+package xieliangji.playwright.examples;
+
+public class Quickstart {
+
+    public static void main(String[] args) {
+        System.out.println("this is quickstart app");
+    }
 }
--- src/test/java/xieliangji/playwright/examples/QuickstartTest.java
+++ src/test/java/xieliangji/playwright/examples/QuickstartTest.java
@@ -1,2 +1,60 @@
-package xieliangji.playwright.examples;public class QuickstartTest {
+package xieliangji.playwright.examples;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+@DisplayName("快速开始")
+public class QuickstartTest {
+
+    @Test
+    void quickstartTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Nested
+    class PlaywrightTest {
+
+        static final String GOOGLE_URL = "https://google.com/ncr";
+
+        static Playwright playwright;
+        static Browser browser;
+
+        BrowserContext context;
+        Page page;
+
+        @BeforeAll
+        @DisplayName("初始化浏览器")
+        static void setup() {
+            playwright = Playwright.create();
+            browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setSlowMo(50).setHeadless(false));
+        }
+
+        @AfterAll
+        @DisplayName("关闭浏览器")
+        static void teardown() {
+            browser.close();
+            playwright.close();
+        }
+
+        @BeforeEach
+        @DisplayName("初始化页面")
+        void initContextPage() {
+            context = browser.newContext();
+            page = context.newPage();
+        }
+
+        @AfterEach
+        @DisplayName("关闭页面")
+        void releaseContextPage() {
+            page.close();
+            context.close();
+        }
+
+        @Test
+        @DisplayName("打开Google Search页面")
+        void testPageTitle() {
+            page.navigate(GOOGLE_URL);
+            Assertions.assertEquals("Google", page.title());
+        }
+    }
 }
